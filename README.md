763b82b83fa75ab58ed02e7cc58e729fea3a5262
Monica He <ehacinom@gmail.com>
2020-02-23 10:54:52 -0600

    Restructuring repos

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 * Django http://localhost:8000/
 * Django admiin http://localhost:8000/admin/
-* Ghost blog http://localhost:2368/
-* Ghost admin http://localhost:2368/ghost/
--- soba_api/settings.py
+++ soba_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*0ma$9y*h0t0+#1jvxj5r1pbmmhgl217h9z1ijtac5##pk6+t('
+SECRET_KEY = '^_ss27ej&v0r-rfz!0ed3x==!%6(m3q&@(-^877%(%-m=^)b7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,17 +73,23 @@ WSGI_APPLICATION = 'soba_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'bananas',
-        'HOST': 'db',
+        'HOST': 'soba_db',
         'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
